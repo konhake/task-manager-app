9d9cdc5b6e9c175e618667e594651f3dc0d9d102
konhake <jamartins1991@gmail.com>
2025-05-19 17:55:31 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+import { environment } from '../environments/environment';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    // Firebase modules
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA03dpeATNGJXiyfUINZ7dNuuiZRJV2894",
+    authDomain: "taskmanagerapp-823a4.firebaseapp.com",
+    projectId: "taskmanagerapp-823a4",
+    storageBucket: "taskmanagerapp-823a4.firebasestorage.app",
+    messagingSenderId: "606917709405",
+    appId: "1:606917709405:web:6ca7df370b414bc84547b4"
+  }
+};
